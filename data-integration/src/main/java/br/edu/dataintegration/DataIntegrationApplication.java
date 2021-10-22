149b17fdd4f5cb27e8f9865fa38f2478ba607282
CezarFelipe <cezarfelipedasilva@gmail.com>
2021-10-21 23:19:05 -0300

    Configuração spring framework do pacote dos dados da integração.

---
+++ data-integration/src/main/java/br/edu/dataintegration/DataIntegrationApplication.java
@@ -0,0 +1,13 @@
+package br.edu.dataintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataIntegrationApplication.class, args);
+	}
+
+}
