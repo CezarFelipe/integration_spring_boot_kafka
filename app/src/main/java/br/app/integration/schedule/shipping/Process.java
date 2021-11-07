b17631bcd133df9faf35823c4ce530921d6f2687
CezarFelipe <cezarfelipedasilva@gmail.com>
2021-11-07 17:36:45 -0300

    configuração das classes consumer

--- app/src/main/java/br/app/integration/schedule/shipping/Process.java
+++ app/src/main/java/br/app/integration/schedule/shipping/Process.java
@@ -1,12 +1,15 @@
 package br.app.integration.schedule.shipping;
 
+import br.app.core.erp.model.Usuario;
 import br.app.core.rest.service.msg.Response;
 import br.app.kafka.config.ParamConfig;
 import br.app.kafka.producer.Producer;
 
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import sun.rmi.runtime.Log;
 
 @Component
 public class Process {
@@ -29,11 +32,20 @@ public class Process {
     @Autowired
     private br.app.core.rest.service.msg.Response pResponse;
 
+    @Autowired
+    private br.app.core.erp.service.UsuarioService userService;
+
+    @Autowired
+    private br.app.core.erp.service.generic.Request request;
+
+    @Autowired
+    private br.app.core.erp.service.generic.Response response;
 
 
     @Scheduled(fixedDelay = 60000)
     public void execute() throws Exception {
 
+        SendUser();
         String payLoad = GetOrder();
 
         if (payLoad != null) {
@@ -60,6 +72,22 @@ public class Process {
         }
 
         return response.getRestVOResponse().getBody();
+
+    }
+    private void SendUser() throws Exception {
+
+        Gson gson       = new Gson();
+        Usuario usuario = new Usuario();
+
+        usuario.setName("Cezar");
+
+        String json = gson.toJson(usuario);
+
+        request.setRequestAction("Create");
+        request.setRequestBody(json);
+
+        userService.Create(request, response);
+        System.out.println(response.getResponseBody());
 
     }
 
---
+++ app/src/main/java/br/app/kafka/consumer/KafkaConsumerConfig.java
@@ -0,0 +1,51 @@
+package br.app.kafka.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Value("${kafka.server.consumer}")
+    private static String SERVER;
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                "localhost:9092");
+        props.put(
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class);
+        props.put(
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String>
+    kafkaListenerContainerFactory() {
+
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+}
